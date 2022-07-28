ba8eb38e5367ec07caa53061e1f5462be5cbe381
Charles Bushong <charles.bushong@cms.hhs.gov>
2022-07-28 09:40:37 -0400

    BATIAI-719: Fixing kubernetes provider and cluster inputs (#10)

--- eniconfig.tf
+++ eniconfig.tf
@@ -2,20 +2,6 @@
 # # Kubernetes provider configuration
 # ###############################################################################
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = var.cluster_name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-    command     = "aws"
-  }
-}
 
 # resource "kubernetes_manifest" "eniconfig_subnets"{
 
