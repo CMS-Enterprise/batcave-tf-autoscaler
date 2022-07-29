6947e5e8c26e94c73711e612aeea7dc57fa34c2b
Arun Sanna <arunchowdary.sanna@cms.hhs.gov>
2022-07-29 11:48:56 -0700

    reverted changes

--- iam.tf
+++ iam.tf
@@ -1,40 +1,27 @@
-locals {
-  k8s_service_account_namespace = "kube-system"
-  k8s_service_account_name      = "cluster-autoscaler-aws-cluster-autoscaler"
-}
-
-module "iam_assumable_role_admin" {
-  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  create_role                   = true
-  role_name                     = "cluster-autoscaler"
-  provider_url                  = replace(var.oidc_issuer_url, "https://", "")
-  role_policy_arns              = [aws_iam_policy.cluster_autoscaler.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:${local.k8s_service_account_namespace}:${local.k8s_service_account_name}"]
-  role_permissions_boundary_arn = "arn:aws:iam::373346310182:policy/cms-cloud-admin/developer-boundary-policy"
-  role_path                     = "/delegatedadmin/developer/"
-}
-
-resource "aws_iam_policy" "cluster_autoscaler" {
-  name_prefix = "cluster-autoscaler"
-  path        = "/delegatedadmin/developer/"
-  description = "EKS cluster-autoscaler policy for cluster ${var.cluster_name}"
-  policy      = data.aws_iam_policy_document.cluster_autoscaler.json
-}
-
-data "aws_iam_policy_document" "cluster_autoscaler" {
-  statement {
-    sid    = "clusterAutoscalerAll"
-    effect = "Allow"
-    actions = [
-      "autoscaling:DescribeAutoScalingGroups",
-      "autoscaling:DescribeAutoScalingInstances",
-      "autoscaling:DescribeLaunchConfigurations",
-      "autoscaling:DescribeTags",
-      "autoscaling:SetDesiredCapacity",
-      "autoscaling:TerminateInstanceInAutoScalingGroup",
-      "ec2:DescribeLaunchTemplateVersions"
+resource "aws_iam_policy" "batcave_autoscaler" {
+  name = "autoscaler-policy-${var.cluster_name}"
+  path = var.iam_path
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeTags",
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup",
+          "ec2:DescribeLaunchTemplateVersions"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
     ]
+  })
+}
 
-    resources = ["*"]
-  }
+resource "aws_iam_role_policy_attachment" "autoscaler_policy_attachment" {
+  role       = var.self_managed_node_groups.general.iam_role_name
+  policy_arn = aws_iam_policy.batcave_autoscaler.arn
 }
