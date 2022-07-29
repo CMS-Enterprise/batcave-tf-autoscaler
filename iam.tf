ca59c324d40672776bea82814f9e8144ea4b3993
Arun Sanna <arunchowdary.sanna@cms.hhs.gov>
2022-07-29 10:04:15 -0700

    added policy data file

--- iam.tf
+++ iam.tf
@@ -1,3 +1,4 @@
+
 resource "aws_iam_policy" "batcave_autoscaler" {
   name = "autoscaler-policy-${var.cluster_name}"
   path = var.iam_path
@@ -24,4 +25,46 @@ resource "aws_iam_policy" "batcave_autoscaler" {
 resource "aws_iam_role_policy_attachment" "autoscaler_policy_attachment" {
   role       = var.self_managed_node_groups.general.iam_role_name
   policy_arn = aws_iam_policy.batcave_autoscaler.arn
+}
+
+data "aws_iam_policy_document" "cluster_autoscaler" {
+  statement {
+    sid    = "clusterAutoscalerAll"
+    effect = "Allow"
+
+    actions = [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DescribeTags",
+      "ec2:DescribeLaunchTemplateVersions",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "clusterAutoscalerOwn"
+    effect = "Allow"
+
+    actions = [
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "autoscaling:UpdateAutoScalingGroup",
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "autoscaling:ResourceTag/kubernetes.io/cluster/${var.cluster_name}"
+      values   = ["owned"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/enabled"
+      values   = ["true"]
+    }
+  }
 }
