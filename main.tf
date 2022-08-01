6bf4d1bdcc318d4f15e2c829ad0dd1d988b26e5b
justin.lowry <justin@leapfrog.ai>
2022-08-01 12:26:54 -0400

    test

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ data "aws_eks_cluster_auth" "cluster" {
 provider "helm" {
   kubernetes {
     host                   = var.cluster_endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster_auth.cluster.certificate_authority[0].data)
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster_auth.cluster.cluster_certificate_authority_data)
     token = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
 provider "kubernetes" {
   host                   = var.cluster_endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster_auth.cluster.certificate_authority[0].data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster_auth.cluster.cluster_certificate_authority_data)
   token = data.aws_eks_cluster_auth.cluster.token
 }
