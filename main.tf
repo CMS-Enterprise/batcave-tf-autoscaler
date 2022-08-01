1d909a1b0fcee62d616527dc42b9be57699651cf
justin.lowry <justin@leapfrog.ai>
2022-08-01 14:31:48 -0400

    test

--- main.tf
+++ main.tf
@@ -10,7 +10,11 @@ provider "helm" {
   kubernetes {
     host                   = var.cluster_endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.cluster_certificate_authority_data)
-    token = data.aws_eks_cluster_auth.cluster.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      command     = "aws"
+    }
   }
 }
 
