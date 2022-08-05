a6e1dda48762d886de45ef6d36b33ee1f7382fb6
Arun Sanna <arunchowdary.sanna@cms.hhs.gov>
2022-08-05 10:41:15 -0700

    updated as per review

--- iam.tf
+++ iam.tf
@@ -30,7 +30,7 @@ resource "aws_iam_policy" "batcave_autoscaler" {
 module "iam_assumable_role_admin" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   create_role                   = true
-  role_name                     = "cluster-autoscaler"
+  role_name                     = "${var.cluster_name}-cluster-autoscaler"
   provider_url                  = replace(var.cluster_oidc_issuer_url , "https://", "")
   role_policy_arns              = [aws_iam_policy.batcave_autoscaler.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:${local.k8s_service_account_namespace}:${local.k8s_service_account_name}"]
